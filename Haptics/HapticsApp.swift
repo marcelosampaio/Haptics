d415114f61383dcb84f9e5968d1b7568a2c41e73
Marcelo Sampaio <marcelosampaiio1965@gmail.com>
2023-04-20 23:14:04 -0300

    Initial Commit

---
+++ Haptics/HapticsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HapticsApp.swift
+//  Haptics
+//
+//  Created by Marcelo Sampaio on 20/04/23.
+//
+
+import SwiftUI
+
+@main
+struct HapticsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
