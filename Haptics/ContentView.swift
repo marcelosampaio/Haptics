a157ada66382cfded3bd79cc06790cd40e519dc4
Marcelo Sampaio <marcelosampaiio1965@gmail.com>
2023-04-21 01:45:07 -0300

    first commit 🌴

--- Haptics/ContentView.swift
+++ Haptics/ContentView.swift
@@ -4,18 +4,54 @@
 //
 //  Created by Marcelo Sampaio on 20/04/23.
 //
-
+import CoreHaptics
 import SwiftUI
 
 struct ContentView: View {
+    @State private var engine: CHHapticEngine?
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        Text("🌴 Haptics")
+            .font(.largeTitle)
+            .padding()
+            .onAppear(perform: prepareHaptics)
+            .onTapGesture(perform: complexSuccess)
+    }
+}
+
+extension ContentView {
+    func complexSuccess() {
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
+        
+        var events = [CHHapticEvent]()
+        
+        for i in stride(from: 0, to: 1.8, by: 0.001) {
+            let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: Float(i))
+            let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: Float(i))
+            let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: i)
+            events.append(event)
         }
-        .padding()
+        
+        do {
+            let pattern = try CHHapticPattern(events: events, parameters: [])
+            let player = try engine?.makePlayer(with: pattern)
+            try player?.start(atTime: 0)
+        } catch {
+            print("❌ player error: \(error.localizedDescription)")
+        }
+    }
+}
+
+extension ContentView {
+    func prepareHaptics() {
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
+        
+        do {
+            engine = try CHHapticEngine()
+            try engine?.start()
+        } catch {
+            print("❌ engine error: \(error.localizedDescription)")
+        }
     }
 }
 
